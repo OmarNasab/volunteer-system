c1a1cacb9031ecb9ee31a3beccffa02050eb9fc4
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2021-01-23 22:25:02 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("app.html");?>
+    </head>
+    <body>
+        <h1 class="primary">hello world hi darwich </h1>
+        <h1>hi</h1>
+    </body>
+</html>
